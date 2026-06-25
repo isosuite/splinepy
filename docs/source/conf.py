995f630e6622ef4c78d7fbfedaa662c7dd4401a3
clemens.fricke <clemens.david.fricke@tuwien.ac.at>
2026-06-25 10:29:29 +0200

    Fix build for python 3.14 (#494)
    * Chore: Update cibuildwheel to v4.1.0
    
    * Chore: update more actions
    
    * Chore: Remove linux cp14t build since it makes some issues for us. Might come back to that funi does not release cp14t
    
    * Chore: Streamline workflow

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ html_theme_options = {
     "icon_links": [
         {
             "name": "GitHub",
-            "url": "https://github.com/tataratat/splinepy",
+            "url": "https://github.com/isosuite/splinepy",
             "icon": "fa-brands fa-square-github",
         },
         {
